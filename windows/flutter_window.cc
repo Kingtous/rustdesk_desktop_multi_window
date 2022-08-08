2c1e9e6d0e09c504e914d5b17dc04369ebe0e6b0
kingtous <kingtous@qq.com>
2022-08-08 15:18:10 +0800

    feat: windows onDestroy

--- windows/flutter_window.cc
+++ windows/flutter_window.cc
@@ -183,6 +183,11 @@ LRESULT FlutterWindow::MessageHandler(HWND hwnd, UINT message, WPARAM wparam, LP
       break;
     }
     case WM_DESTROY: {
+      // Give onDestory callback to Flutter to close window gracefully
+      if (window_channel_) {
+          auto args = flutter::EncodableValue(flutter::EncodableMap());
+          window_channel_->InvokeMethod(0, "onDestory", &args);
+      }
       Destroy();
       if (!destroyed_) {
         destroyed_ = true;
